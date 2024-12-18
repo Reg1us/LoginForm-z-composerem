eea3ce96948c4bac0c4a837012e3e2b0d3ccb485
Reg1us <lasockijakuba@gmail.com>
2024-12-18 18:53:49 +0100

    LoginForm z composerem

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require 'vendor/autoload.php';
+require_once('class/User.php');
+session_start(); 
+use Steampixel\Route;
+
+Route::add('/', function() {
+    include('templates/index.php');
+});
+Route::add('/login', function() {
+    include('templates/login.php');
+});
+Route::add('/login', function() {
+    include('templates/login.php');
+}, 'post');
+Route::add('/register', function() {
+    include('templates/register.php');
+});
+Route::add('/register', function() {
+    include('templates/register.php');
+}, 'post');
+Route::add('/logout', function() {
+    include('templates/logout.php');
+});
+
+Route::run('/phploginform-main');
+?>
